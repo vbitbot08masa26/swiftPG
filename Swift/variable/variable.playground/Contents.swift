9200ad10d891e8a98204c0826156df44f77acd19
vbitbot08masa26 <hiroike0826masaya2020@gmail.com>
2021-04-01 09:23:00 +0900

    ver.2021.04.01.09.22

---
+++ Swift/variable/variable.playground/Contents.swift
@@ -0,0 +1,17 @@
+var value1 = "ABC"
+let value2 = "DEF"
+
+value1 = "GHI"
+value2 = "JK"   // error
+
+var value3: String
+let value4: String
+
+value3 = "GHI"
+value4 = "JK"
+
+var value5: String = "abc"
+let value6: String = "def"
+
+value5 = "ghi"
+value6 = "jk" // error
