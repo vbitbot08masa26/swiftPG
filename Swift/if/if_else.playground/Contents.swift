417b48ec4d8812fd49de0cfe58b5cf572ede58a4
vbitbot08masa26 <hiroike0826masaya2020@gmail.com>
2021-04-08 22:59:20 +0900

    ver.2021.04.08.22.59

--- Swift/if/if_else.playground/Contents.swift
+++ Swift/if/if_else.playground/Contents.swift
@@ -1,11 +1,21 @@
 var i: Int = 1;
-var msg: String
+var msg1: String
+var msg2: String
 
-if(i == 1){
-    msg = "i is 1"
+if(i == 1) {
+    msg1 = "i is 1"
+}
+else {
+    msg1 = "i is not 1"
+}
+
+msg1    //  "i is 1"
+
+if i == 1{
+    msg2 = "i is 1"
 }
 else{
-    msg = "i is not 1"
+    msg2 = "i is not 1"
 }
 
-msg //  "i is 1"
+msg2    //  "i is 1"
