47ced7a1a0d7093abfe17fff872f16cffcce34da
vbitbot08masa26 <hiroike0826masaya2020@gmail.com>
2021-04-09 09:11:03 +0900

    ver.2021.04.09.09.10

---
+++ Swift/while/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+var i = 1
+var sum = 0
+
+while i <= 10 {
+    sum += i
+    i += 1
+}
+
+sum //  55
+
+//----------------------------
+
+i = 1
+sum = 0
+
+repeat{
+    sum += i
+    i += 1
+} while i <= 10
+
+sum //  55
